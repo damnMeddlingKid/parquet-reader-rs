7f396b6256422a738db19ca5dee005f90cadd306
Franklyn Dsouza <franklyn.dsouza@shopify.com>
2023-02-11 17:55:55 -0500

    only do extra read if necessarY

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use parquet_format_safe::{FileMetaData, PageHeader, Type};
 use std::{fs::File, result::Result};
 use std::io::{Read, Seek, SeekFrom, Error};
 
-const STARTING_READ_SIZE: usize = 64;
+const STARTING_READ_SIZE: usize = 1024;
 const MAGIC_NUMBER : [u8; 4] = [b'P', b'A', b'R', b'1'];
 
 fn read_footer(reader: &mut File) -> Result<FileMetaData, Box<dyn std::error::Error>> {
@@ -23,14 +23,21 @@ fn read_footer(reader: &mut File) -> Result<FileMetaData, Box<dyn std::error::Er
 
     reader.seek(SeekFrom::End(-8 -(file_metadata_size as i64)))?;
 
-    buffer.clear();
-    buffer.try_reserve((file_metadata_size as usize) - STARTING_READ_SIZE)?;
+    let metadata: &[u8];
 
-    reader.take(file_metadata_size as u64).read_to_end(&mut buffer)?;
+    if file_metadata_size as usize > buffer.capacity() {
+        // if the buffer is not big enough lets reserver more capacity
+        buffer.try_reserve((file_metadata_size as usize) - STARTING_READ_SIZE)?;
+        buffer.resize(file_metadata_size as usize, 0);
+        reader.read_exact(&mut buffer)?;
+        metadata = &buffer;
+    } else {
+        // If the buffer is too big lets take a slice of the bytes read
+        let remaining = buffer.len() - file_metadata_size as usize;
+        metadata = &buffer[remaining..]
+    }
 
-    let reader: &[u8] = &buffer;
-
-    let mut protocol = TCompactInputProtocol::new(reader, (file_metadata_size as usize)*2);
+    let mut protocol = TCompactInputProtocol::new(metadata, (file_metadata_size as usize)*2);
 
     Ok(FileMetaData::read_from_in_protocol(&mut protocol).unwrap())
 }
@@ -46,41 +53,38 @@ fn read_column(reader: &mut File, page_offset: u64, column_type: Type) -> Result
     let num_values = page_header.data_page_header.unwrap().num_values as usize;
     let compressed_size = page_header.compressed_page_size as usize;
     let uncompressed_size = page_header.uncompressed_page_size as usize;
-    let mut buffer = Vec::with_capacity(compressed_size);
+    let mut compressed_buffer = Vec::with_capacity(compressed_size);
 
     // TODO Reuse this buffer for decompressing other pages
     let mut output_buffer = vec![0u8; uncompressed_size];
 
-    let bytes_read = reader
-    .take(compressed_size as u64)
-    .read_to_end(&mut buffer).unwrap();
+    let bytes_read = match reader.take(compressed_size as u64).read_to_end(&mut compressed_buffer) {
+        Ok(bytes) => bytes,
+        Err(err) => return Err(Box::new(err)),        
+    };
 
     if bytes_read != compressed_size {
-        println!("Read was the wrong size");
+        return Err(Box::new(std::io::Error::new(std::io::ErrorKind::Other, "Read was the wrong size")));
     }
-
-    let reader: &[u8] = &buffer;
     
-    let mut decoder = zstd::Decoder::new(reader)?;
+    let mut decoder = zstd::Decoder::new(&*compressed_buffer)?;
     decoder.read_exact(&mut output_buffer)?;
 
     match column_type {
         Type::INT64 => {
             let values_start = uncompressed_size - (num_values * 8);
-            read_plain_int64(&output_buffer[values_start..])
+            read_plain_int64(&output_buffer[values_start..])?
         }
-        _ => read_plain_int64(&output_buffer)
+        _ => return Err(Box::new(std::io::Error::new(std::io::ErrorKind::Other, "Unknown Column Encoding")))
     };
 
     Ok(())
 }
 
 fn read_plain_int64(buffer: &[u8]) -> Result<(), Box<dyn std::error::Error>> {
-    let mut i = 0;
-    while i + 8 <= buffer.len()  {
-        let value = u64::from_le_bytes(buffer[i..i +8].try_into()?);
+    for chunk in buffer.chunks_exact(8) {
+        let value = u64::from_le_bytes(chunk.try_into()?);
         println!("{:?}\n", value);
-        i += 8;
     }
     Ok(())
 }
